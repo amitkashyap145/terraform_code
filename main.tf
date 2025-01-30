02f14ab13de33cb9a6c6283eb687d9ab39db9770
DevOps Engineer <92769132+amitkashyap145@users.noreply.github.com>
2025-01-30 16:59:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,8 @@
-resource "cloudstack_instance" "sonarqube" {
-  name             = "sonarqube"                                # this is vm name
+resource "cloudstack_instance" "terraformcloud" {
+  name             = "terraformcloud"                                # this is vm name
   service_offering = "medium"                                   # this is service offering
   network_id       = "d4bcfcc3-4f9f-491d-a444-92bdca351cc3"    # this is network id for the 10.1.75.0 network
   template         = "storage"                               # this is template name of centos image
   zone             = "zone1"                                 # this is just the zone-name mentioned in clooudstack
 }
 
-resource "cloudstack_instance" "nexus" {
-  name             = "nexus"                                # this is vm name
-  service_offering = "medium"                                   # this is service offering
-  network_id       = "d4bcfcc3-4f9f-491d-a444-92bdca351cc3"    # this is network id for the 10.1.75.0 network
-  template         = "storage"                               # this is template name of centos image
-  zone             = "zone1"                                 # this is just the zone-name mentioned in clooudstack
-}
