6b482db8bcfce1827300cca6ed387deac1cf57c9
DevOps Engineer <92769132+amitkashyap145@users.noreply.github.com>
2025-01-31 17:20:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "cloudstack_instance" "terraformcloud" {
-  name             = "terraformcloud"                                # this is vm name
+resource "cloudstack_instance" "cloudstackinstance1" {
+  name             = "cloudinstance1"                                # this is vm name
   service_offering = "medium"                                   # this is service offering
   network_id       = "d4bcfcc3-4f9f-491d-a444-92bdca351cc3"    # this is network id for the 10.1.75.0 network
   template         = "storage"                               # this is template name of centos image
